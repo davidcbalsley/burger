b4cf61b2a91e0cb7d009c786afe6df243155ebdc
David Joseph <56838866+davidcbalsley@users.noreply.github.com>
2020-03-24 11:51:08 -0500

    adding schema.sql and seeds.sql to db folder

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+-- Add a record to the burgers table for a hamburger
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Hamburger", false);
+
+-- Add a record to the burgers table for a cheeseburger
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Cheeseburger", false);
+
+-- Add a record to the burgers table for a mushroom swiss burger
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Mushroom Swiss Burger", false);
