d4bf1b68fcbcbca5f19ad278d810d284ee9bcbf3
David Joseph <56838866+davidcbalsley@users.noreply.github.com>
2020-03-24 14:53:56 -0500

    fixing typo with port in connection.js

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ var mysql = require("mysql");
 
 var connection = mysql.createConnection({
   host: "localhost",
-  port: process.env.PORT || 3306,
+  port: 3306,
   user: "root",
   password: "",
   database: "burger_db"
