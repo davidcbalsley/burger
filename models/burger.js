7bc36a846d5b2d3a6bb0f000aa7f1d4acde03901
David Joseph <56838866+davidcbalsley@users.noreply.github.com>
2020-03-24 14:24:36 -0500

    shell for model

---
+++ models/burger.js
@@ -0,0 +1,10 @@
+var orm = require("../config/orm.js");
+
+var burger = {
+    all: function(cb) {
+        // Nothing yet
+    }
+}
+
+// Export the database functions for the controller
+module.exports = burger;
