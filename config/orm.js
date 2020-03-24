69fd9318e286581bcd5b9cc729ae375c0b98bbc8
David Joseph <56838866+davidcbalsley@users.noreply.github.com>
2020-03-24 14:23:12 -0500

    code for connecting to MySQL and stubs for ORM

---
+++ config/orm.js
@@ -0,0 +1,16 @@
+// Import MySQL connection
+var connection = require("../config/connection.js");
+
+var orm = {
+    selectAll: function() {
+        // Nothing yet   
+    },
+    insertOne: function() {
+        // Nothing yet
+    },
+    updateOne: function() {
+        // Nothing yet
+    }
+}
+
+module.exports = orm;
