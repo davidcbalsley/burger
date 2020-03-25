a9db9223ca3bfff314abfa892d120fff25b89e5c
David Joseph <56838866+davidcbalsley@users.noreply.github.com>
2020-03-25 01:25:34 -0500

    updating burgers and clicking devour it button

--- config/orm.js
+++ config/orm.js
@@ -11,6 +11,24 @@ function printQuestionMarks(num) {
     return arr.toString();
 }
 
+function objToSql(ob) {
+    var arr = [];
+
+    for (var key in ob) {
+        var value = ob[key];
+
+        if (Object.hasOwnProperty.call(ob, key)) {
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+
+            arr.push(key + "=" + value);
+        }
+    }
+
+    return arr.toString();
+}
+
 var orm = {
     selectAll: function(table, cb) {
         var queryString = "SELECT * FROM " + table + ";";
@@ -39,8 +57,22 @@ var orm = {
             cb(result);
         });
     },
-    updateOne: function() {
-        // Nothing yet
+    updateOne: function(table, objColVals, condition, cb) {
+        var queryString = "UPDATE " + table;
+
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        })
+
     }
 }
 
--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,5 +1,24 @@
 $(function() {
 
+    $(".devour-it").on("click", function(event) {
+        var id = $(this).data("id");
+
+        var newDevouredState = {
+            devoured: 1
+        };
+
+        // Send the PUT request
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: newDevouredState
+        }).then(
+            function() {
+                // Reload the page to get the updated list
+                location.reload();
+            }
+        );
+    })
+
     $(".create-form").on("submit", function(event) {
         event.preventDefault();
 
